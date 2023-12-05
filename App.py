0f3f7317ac3983a568135aa43aef00d5d5dab2ad
Sivan22 <89018301+Sivan22@users.noreply.github.com>
2023-12-05 23:40:33 +0000

    update

--- App.py
+++ App.py
@@ -1,13 +1,25 @@
 import streamlit as st
 from streamlit.logger import get_logger
 from transformers import  pipeline
+import datasets
+import pandas as pd
+from huggingface_hub import login
 
 LOGGER = get_logger(__name__)
-model = "sivan22/halacha-siman-classifier"
+model = "sivan22/halacha-siman-seif-classifier-new"
+
+
+login('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')
+ds = datasets.load_dataset('sivan22/orach-chaim',token=True)
+df = ds['train'].to_pandas()
+def clean(s)->str:
+    return s.replace(" ","")
+df['seif']= df['seif'].apply(clean)
+
 
 
 def get_predicts_local(input)->str:
-    classifier = pipeline("text-classification",model=model,top_k=5)
+    classifier = pipeline("text-classification",model=model,top_k=None)
     predicts = classifier(input)
     return predicts
 
@@ -16,9 +28,7 @@ def get_predicts_online(input)->str:
     API_URL = "https://api-inference.huggingface.co/models/" + model
     headers = {"Authorization": f"Bearer {'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'}"}
     def query(input_text):
-        response = requests.post(API_URL, headers=headers, json='{inputs:' +input_text+'}')
-        if response.status_code == 503:
-            response = requests.post(API_URL, headers=headers, json='{{inputs:' +input_text+'}{wait_for_model:true}{top_k:5}}')        
+        response = requests.post(API_URL, headers=headers, json='{{inputs:' +input_text+'}{wait_for_model:true}}')        
         return response.json()
     predicts = query(input)
     return predicts
@@ -32,14 +42,18 @@ def run():
     st.write("# חיפוש בשולחן ערוך")
     use_local = st.checkbox("חיפוש לא מקוון")
     user_input = st.text_input('כתוב כאן את שאלתך', placeholder='כמה נרות מדליקים בחנוכה')
-    if st.button('השב'):
-        if(user_input!="" ):
-            if use_local:
-                for prediction in get_predicts_local(user_input)[0][:5]:
-                        st.write('סימן ' + str(prediction['label']))
-            else:            
-                for prediction in get_predicts_online(user_input)[0][:5]:
-                    st.write('סימן ' + str(prediction['label']))
+    if st.button('חפש') and user_input!="":       
+        get_predicts = get_predicts_local if use_local else get_predicts_online
+        #print(get_predicts(user_input)[0][0:5])
+        for prediction in get_predicts(user_input)[0][:5]:
+            rows = df[((df["bookname"] == " שלחן ערוך - אורח חיים ") |
+                        (df["bookname"] ==" משנה ברורה")) &
+                      (df["siman"] == prediction['label'].split(' ')[0])&
+                      (df["seif"] == prediction['label'].split(' ')[1]) ]
+            rows.sort_values(["bookname"],ascending=False, inplace=True) 
+         
+            print(prediction['label'].split(' '))
+            st.write('סימן ' + str(prediction['label']), rows[['text','sek','seif','siman','bookname']])
 
     
 
