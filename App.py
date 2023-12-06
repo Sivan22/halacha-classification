6f0701b4f8f621453e69493dc93dfdf9d6d4b5e1
Sivan22 <89018301+Sivan22@users.noreply.github.com>
2023-12-06 14:41:04 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,13 +3,15 @@ from streamlit.logger import get_logger
 from transformers import  pipeline
 import datasets
 import pandas as pd
-from huggingface_hub import login
+
 
 LOGGER = get_logger(__name__)
 model = "sivan22/halacha-siman-seif-classifier"
 
 
-ds = datasets.load_dataset('sivan22/orach-chaim')
+login('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')
+ds = datasets.load_dataset('sivan22/orach-chaim',token=True)
+ds.save_to_disk('sivan22/orach-chaim')
 df = ds['train'].to_pandas()
 def clean(s)->str:
     return s.replace(" ","")
@@ -17,15 +19,16 @@ df['seif']= df['seif'].apply(clean)
 
 
 
-def get_predicts_local(input)->str:
+def get_predicts(input)->str:
     classifier = pipeline("text-classification",model=model,top_k=None)
-    predicts = classifier(input)
+    classifier.save_pretrained(model)
+    predicts = classifier(model)
     return predicts
 
 def get_predicts_online(input)->str:
     import requests
     API_URL = "https://api-inference.huggingface.co/models/" + model
-    headers = {"Authorization": f"Bearer {'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'}"}
+    headers = {"Authorization": f"Bearer {'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'}"}
     def query(input_text):
         response = requests.post(API_URL, headers=headers, json='{{inputs:' +input_text+'}{wait_for_model:true}}')        
         return response.json()
@@ -39,19 +42,14 @@ def run():
     )
 
     st.write("# חיפוש בשולחן ערוך")
-    use_local = st.checkbox("חיפוש לא מקוון")
     user_input = st.text_input('כתוב כאן את שאלתך', placeholder='כמה נרות מדליקים בחנוכה')
     if st.button('חפש') and user_input!="":       
-        get_predicts = get_predicts_local if use_local else get_predicts_online
-        #print(get_predicts(user_input)[0][0:5])
         for prediction in get_predicts(user_input)[0][:5]:
             rows = df[((df["bookname"] == " שלחן ערוך - אורח חיים ") |
                         (df["bookname"] ==" משנה ברורה")) &
                       (df["siman"] == prediction['label'].split(' ')[0])&
                       (df["seif"] == prediction['label'].split(' ')[1]) ]
             rows.sort_values(["bookname"],ascending=False, inplace=True) 
-         
-            print(prediction['label'].split(' '))
             st.write('סימן ' + str(prediction['label']), rows[['text','sek','seif','siman','bookname']])
 
     
